88059231a7ad7cba0c002a09c6b75a00de5df3c1
admin <matthewosu@gmail.com>
2021-07-03 13:19:13 -0700

    object is mapped and rendering

--- expressapp/spelling-app/src/components/dictpage.js
+++ expressapp/spelling-app/src/components/dictpage.js
@@ -1,18 +1,19 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
-export default function DictPage () {
-    const [word, setWord] = useState([])
+export default function DictPage() {
+  const [words, setWords] = useState([]);
 
-    useEffect(() => {
-        console.log("You made it")
-        fetch("/spellinglist")
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data, "data");
-            setWord(data.dictionary);
-          });
-      }, []);
-    
+  useEffect(() => {
+    console.log("You made it");
+    fetch("/spellinglist")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, "data");
+        setWords(data.dictionary);
+        console.log(data.dictionary, "d.d");
+      });
+  }, []);
+
   // useEffect(() => {
   //   const options = {
   //     method: "GET",
@@ -25,29 +26,59 @@ export default function DictPage () {
   //     .then(data => {
   //       console.log(data, 'cats')
   //     })
-     
+
   // })
 
-    return(
-        <>
-        <div id="dict-page">
-            <div id="missed-column">
-                <p>Recently Missed Words</p>
-            </div>
-            <div id="study-column">
-                <p>Study Words</p>
-            </div>
-            <div id="custom-column">
-                <p>Custom Study List</p>
-                <form>
-                    <select id="custom-selects">
-                        <option value="Animals">Animals</option>
-                        <option value="Long Words">Long Words</option>
-                    </select>
-                </form>
-                {/* <button onClick={() => }></button> */}
-            </div>  
+  function Delete() {
+      
+  }
+
+  return (
+    <>
+      <div id="dict-page">
+        <div id="missed-column">
+          <p id="missed-column-p">Recently Missed Words</p>
+          {words.map((i) => {
+            if (i == null) {
+            } else {
+              return (
+                  <div id="misssed-row">
+                <p id="missed-column-data">
+                   {i.word}: {i.typo}
+                </p>
+                <button id="missed-btn" onClick={() => Delete}>remove</button>
+                </div>
+              );
+            }
+          })}
         </div>
-        </>
-    )
+        <div id="study-column">
+          <p>Study Words</p>
+        </div>
+        <div id="custom-column">
+          <p id="custom-column-p">Custom Study List</p>
+          <form>
+            <select id="custom-selects">
+              <option value="Animals">Animals</option>
+              <option value="Long Words">Long Words</option>
+            </select>
+          </form>
+          <button onClick={() => console.log(words)}></button>
+
+          {words.map((i) => {
+            if (i == null) {
+            } else {
+              return (
+                  <>
+                <p>
+                  {i.typo} {i.word} 
+                </p>
+                </>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
